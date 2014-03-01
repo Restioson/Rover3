79c15719a9862b1bc5ee797289451d77fe0c878b
Restioson <cael.marquard@gmail.com>
2014-03-01 15:50:07 +0200

    0.2.0
    New update!

--- Rover3.py
+++ Rover3.py
@@ -1,23 +1,25 @@
 import sys
 import datetime
-import ctypes
-import ctypes.utils
+#import ctypes
+#import ctypes.utils
 import time
+
+### _linux_set_time taken from stackoverflow.com
 
 def _linux_set_time(time_tuple):
 	CLOCK_REALTIME = 0
- 	class timespec(ctypes.Structure):
-       		 _fields_ = [("tv_sec", ctypes.c_long),
-                   	    ("tv_nsec", ctypes.c_long)]
+	class timespec(ctypes.Structure):
+			_fields_ = [("tv_sec", ctypes.c_long),
+						("tv_nsec", ctypes.c_long)]
 
-    		librt = ctypes.CDLL(ctypes.util.find_library("rt"))
+			librt = ctypes.CDLL(ctypes.util.find_library("rt"))
 	
-    		ts = timespec()
-    		ts.tv_sec = int( time.mktime( datetime.datetime( *time_tuple[:6]).timetuple() ) )
-   	 	ts.tv_nsec = time_tuple[6] * 1000000 # Millisecond to nanosecond
+			ts = timespec()
+			ts.tv_sec = int( time.mktime( datetime.datetime( *time_tuple[:6]).timetuple() ) )
+			ts.tv_nsec = time_tuple[6] * 1000000 # Millisecond to nanosecond
 
-    		# http://linux.die.net/man/3/clock_settime
-    		librt.clock_settime(CLOCK_REALTIME, ctypes.byref(ts))
+			# http://linux.die.net/man/3/clock_settime
+			librt.clock_settime(CLOCK_REALTIME, ctypes.byref(ts))
 
 class Rover3():
 	def __init__(self):
@@ -35,15 +37,18 @@ class Rover3():
 		self.minute = ''
 		self.second = ''
 		self.milisecond = ''
+		self.time_ = '' 
+		self.time = ''
 	
 	def parse(self):
 		string = self.data
-		string = string.replace(',',' ')
 		string = string.split()
-		
 		self.tag = string.pop(0)
+		string = ''+string[0]+' '+string[1]
 		
 		if self.tag == 'GPS':
+			string = string.replace(',',' ')
+			string = string.split()
 			self.latitude = string.pop(0)
 			self.longitude = string.pop(0)
 			self.course = string.pop(0)
@@ -51,6 +56,9 @@ class Rover3():
 			self.heading = string.pop(0)
 		
 		if self.tag == 'TIME':
+			string = string.replace(':',' ')
+			string = string.replace('-',' ')
+			string = string.split()
 			self.year = int(string.pop(0))
 			self.month = int(string.pop(0))
 			self.day = int(string.pop(0))
@@ -59,4 +67,9 @@ class Rover3():
 			self.second = int(string.pop(0))
 			self.milisecond = int(string.pop(0))
 			time_tuple = (self.year,self.month,self.day,self.hour,self.minute,self.second,self.milisecond)
-			_linux_set_time(time_tuple)
+			self.time_ = ('Year:'+str(self.year)+' Month:'+str(self.month)+' Day:'+str(self.day)+' Hour:'+str(self.hour)+' Minute:'+str(self.minute)+' Second:'+str(self.second))
+			#_linux_set_time(time_tuple)
+TIME = Rover3()
+TIME.data = 'TIME 2014-03-1 16:27:00:000'
+TIME.parse()
+print(TIME.time_)
