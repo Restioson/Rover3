1847eb2f8b25d8c64e94cd9525c220c19ce6099e
Restioson <Restioson@users.noreply.github.com>
2014-03-01 15:08:59 +0200

    Update ArduinoCom.py

--- Python/ArduinoCom.py
+++ Python/ArduinoCom.py
@@ -10,38 +10,38 @@ state = 'ON'
 gps = Rover3.Rover3()
 f = ''
 def check():
-try:
-while True:
-rdln = ser.readline()
-if rdln != 'BREAK serial':
-gps.data = rdln
-gps.parse()
-if rdln == 'BREAK serial':
-state == 'OFF'
-ser.write('BREAK serial confirm')
-sys.exit()
-except KeyboardInterrupt:
-f.close()
-ser.close()	
+	try:
+		while True:
+			rdln = ser.readline()
+			if rdln != 'BREAK serial':
+				gps.data = rdln
+				gps.parse()
+			if rdln == 'BREAK serial':
+				state == 'OFF'
+				ser.write('BREAK serial confirm')
+				sys.exit()
+	except KeyboardInterrupt:
+		f.close()
+		ser.close()		
 def mainloop():
-f = open('RoverMission.log','ab')
-while state=='ON':
-try:
-time.sleep(10)	
-f.write('Latitude: '+gps.latitude)
-f.write(' Longitude: '+gps.longitude)
-f.write(' Course: '+gps.course)
-f.write(' Speed: '+gps.speed)
-f.write(' Heading: '+gps.heading)
-f.write('\n')
-print('Latitude:'+gps.latitude)
-print('Longitude:'+gps.longitude)
-print('Course:'+gps.course)
-print('Speed:'+gps.speed)
-print('Heading:'+gps.heading)
-except KeyboardInterrupt:
-f.close()
-break
-ser.close()
+	f = open('RoverMission.log','ab')
+	while state=='ON':
+		try:
+			time.sleep(10)	
+			f.write('Latitude: '+gps.latitude)
+			f.write(' Longitude: '+gps.longitude)
+			f.write(' Course: '+gps.course)
+			f.write(' Speed: '+gps.speed)
+			f.write(' Heading: '+gps.heading)
+			f.write('\n')
+			print('Latitude:'+gps.latitude)
+			print('Longitude:'+gps.longitude)
+			print('Course:'+gps.course)
+			print('Speed:'+gps.speed)
+			print('Heading:'+gps.heading)
+		except KeyboardInterrupt:
+			f.close()
+			break
+			ser.close()
 thread.start_new_thread(check,())
 mainloop()
