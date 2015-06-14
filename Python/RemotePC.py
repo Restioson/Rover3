81c9dd9e54f8f506716f2baf387ca63a242611a1
Restioson <cael.marquard@gmail.com>
2015-06-14 07:40:16 +0200

    Fixed Remote
    It now also has a system for handling computer crashes!

--- Python/RemotePC.py
+++ Python/RemotePC.py
@@ -4,6 +4,7 @@ import serial
 import sys
 import pygame
 import thread
+import time
 print('Starting...')
 print("Sometimes the ports get wacky! Mine is COM8!")
 port = int(input('Port > '))-1
@@ -16,52 +17,62 @@ def read(a,b):
     global text
     while True:
         text = ser.read(512)
-        #print text
+def alive(a,b):
+    global ser
+    while True:
+        ser.write('a')
+        time.sleep(1)
+thread.start_new_thread(alive,('a','b'))
 thread.start_new_thread(read,('a','b'))
 myfont = pygame.font.SysFont("monospace", 15)
 text = ''
-label = myfont.render(text, 12, (255,255,255))
+label = myfont.render(text, 12, (0,0,0))
 screen.blit(label, (0, 0))
+lastcmd = ''
 while True: 
         for event in pygame.event.get(): #Event loop
             keyinput = pygame.key.get_pressed()
+
             if keyinput[pygame.K_UP]:
-                 ser.write('u')
-                 print('forward')
+                ser.write('u')
+                print('forward')
+                lastcmd = 'm'
+                 
             if keyinput[pygame.K_DOWN]:
                 ser.write('d')
                 print('back')
+                lastcmd = 'm'
+                
             if keyinput[pygame.K_LEFT]:
                 ser.write('l')
                 print('left')
+                lastcmd = 'm'
+                
             if keyinput[pygame.K_RIGHT]:
                 ser.write('r')
                 print('right')
-            if keyinput[pygame.K_RETURN]:
-                ser.write('E')
-                print('enter')
+                lastcmd = 'm'
+                
+#            if keyinput[pygame.K_RETURN]:
+#                print('enter')
 
-            if keyinput[pygame.K_UP] == False:
-                ser.write('e')
-                print('end')
-            if keyinput[pygame.K_DOWN] == False:
-                ser.write('e')
-                print('end')
-            if keyinput[pygame.K_LEFT] == False:
-                ser.write('e')
-                print('end')
-            if keyinput[pygame.K_RIGHT] == False:
-                ser.write('e')
-                print('end')
-            if keyinput[pygame.K_RETURN] == False:
-                ser.write('e')
-                print('end')
+            if keyinput[pygame.K_UP] == False and keyinput[pygame.K_DOWN] == False and keyinput[pygame.K_LEFT] == False and keyinput[pygame.K_RIGHT] == False:
+                end = True
+                
             if event.type == pygame.QUIT:
                 ser.write('e')
                 ser.close()
                 sys.exit(0)
+            
+            if lastcmd != 'e' and end == True:
+                print('Sending end cmd')
+                ser.write('e')
+                lastcmd = 'e'
+                
+            end = False
 
-        screen.blit(label, (0, 0))
+        screen.blit(myfont.render(text, 12, (0,0,0)), (0, 0))
+        print(text)
         pygame.display.flip()
 
                 
