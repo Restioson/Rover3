68afef115aef94f3d5280eab761642aace56e831
Restioson <restiosondev@gmail.com>
2016-06-25 12:24:44 +0200

    Added socket server for streaming

--- Python/main.py
+++ Python/main.py
@@ -2,6 +2,9 @@
 import time
 import log
 import arduinoserial
+import socketserver
+import subprocess
+import threading
 
 #Main class
 class Main():
@@ -9,35 +12,60 @@ class Main():
     #Initialiser
     def __init__(self):
         
-        #Initialise Rover class
-        self.rover = rover3.Rover3()
+        #Server
+        self.server = socketserver.TCPServer(("0.0.0.0", "1895"), RequestHandler)
         
         #Initialise logger
         self.logger = log.Logger("/home/pi/log/")
         
         #Initialise serial
-        self.serial = arduinoserial.ArduinoSerialConnection()
+        try:    
+            self.serial = arduinoserial.ArduinoSerialConnection()
+        except:
+            self.serial = False
     
     #Mainloop of program
     def mainloop(self):
         
+        #Start server thread
+        serverThread = threading.Thread(target=self.server.serve_forever)
+        serverThread.daemon = True
+        serverThread.start()
+        
         #Loop
-        while True:
+        if self.serial != False:
+            while True:
             
-            #Waits for data and then parses
-            data = self.serial.parse()
+                #Waits for data and then parses
+                data = self.serial.parse()
             
-            #Create message format
-            logmessageFormat = "Latitude: {0}\nLongitude: {1}\nAltitude: {2} Course: {3}\nHeading: {4}\nSpeed: {5}\nTemperature: {5}\nHumidity: {7}\nPitch: {8}\nRoll: {9}\nObjectDistanceFront: {10}\nObjectDistanceBack: {11}\nOther: {12}"
+                #Create message format
+                logmessageFormat = "Latitude: {0}\nLongitude: {1}\nAltitude: {2} Course: {3}\nHeading: {4}\nSpeed: {5}\nTemperature: {5}\nHumidity: {7}\nPitch: {8}\nRoll: {9}\nObjectDistanceFront: {10}\nObjectDistanceBack: {11}\nOther: {12}"
             
-            #Create message
-            logmessage = messageFormat.format(data["latitude"], data["longitude"], data["altitude"], data["course"], data["heading"], data["speed"], data["temperature"], data["humidity"], data["pitch"], data["roll"], data["objdistfront"], data["objdistback"], data["other"])
+                #Create message
+                logmessage = messageFormat.format(data["latitude"], data["longitude"], data["altitude"], data["course"], data["heading"], data["speed"], data["temperature"], data["humidity"], data["pitch"], data["roll"], data["objdistfront"], data["objdistback"], data["other"])
             
-            #Create header
-            logheader = "[{0}:{1}:{2}_{3}-{4}-{5}]".format(data["hour"], data["minute"], data["second"], data["year"], data["month"], data["day"])
+                #Create header
+                logheader = "[{0}:{1}:{2}_{3}-{4}-{5}]".format(data["hour"], data["minute"], data["second"], data["year"], data["month"], data["day"])
+                
+                #Log
+                self.logger.log(logmessage, logheader)
+
             
-            #Log
-            self.logger.log(logmessage, logheader)
+#Request handler class
+class RequestHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        self.data = self.request.recv(1024)
+        
+        if self.data == "start":
+            subprocess.call(["/etc/init.d/uv4l_raspicam", "start"])
+        
+        elif self.data == "stop":
+            subprocess.call(["/etc/init.d/uv4l_raspicam", "stop"])
+
+            
+        
 
 if __name__ == "__main__":
     
